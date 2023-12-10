e7c1ee786cbdff81530ffbf706b914c53d684a00
thomlaterreur <109108228+thomlaterreur@users.noreply.github.com>
2023-12-10 18:46:17 +0100

    r should be working
    run with
    Rscript src/models/CODE_FAMA_CURRENCY.R

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Python
-conda create --name venv python=3.10
-conda activate venv
+conda create --name venv3 --use-index-cache python=3.10
+conda activate venv3
 conda install -c conda-forge --file requirements.txt
 
