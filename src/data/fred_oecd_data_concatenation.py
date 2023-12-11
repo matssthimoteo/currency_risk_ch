70ac4391363e42533a15380eef46564803d33e1a
mateusst28 <msthimote@gmail.com>
2023-12-11 03:10:55 +0100

    modularize oecd and merge

--- src/data/fred_oecd_data_concatenation.py
+++ src/data/fred_oecd_data_concatenation.py
@@ -1,20 +1,25 @@
 import pandas as pd
 
-# Load separate data FRED/OECD
-fred_df = pd.read_csv('data/interim/monthly_data_fred.csv')
-oecd_df = pd.read_csv('data/interim/monthly_data_oecd.csv')
 
-# Handle timestamps
-assert 'date' in fred_df.columns, "Bad FRED csv formatting."
-fred_df.rename(columns={'date': 'Date'}, inplace=True)
-assert 'Date' in oecd_df.columns, "Bad OECD csv formatting."
-fred_df['Date'] = fred_df['Date'].map(lambda x: pd.Timestamp(x))
-oecd_df['Date'] = oecd_df['Date'].map(lambda x: pd.Timestamp(x))
-fred_df.set_index('Date', inplace=True)
-oecd_df.set_index('Date', inplace=True)
+def merge():
+    # Load separate data FRED/OECD
+    fred_df = pd.read_csv('data/interim/monthly_data_fred.csv')
+    oecd_df = pd.read_csv('data/interim/monthly_data_oecd.csv')
 
-# Concatenate data
-df = pd.concat([fred_df, oecd_df], axis=1)
+    # Handle timestamps
+    assert 'date' in fred_df.columns, "Bad FRED csv formatting."
+    fred_df.rename(columns={'date': 'Date'}, inplace=True)
+    assert 'Date' in oecd_df.columns, "Bad OECD csv formatting."
+    fred_df['Date'] = fred_df['Date'].map(lambda x: pd.Timestamp(x))
+    oecd_df['Date'] = oecd_df['Date'].map(lambda x: pd.Timestamp(x))
+    fred_df.set_index('Date', inplace=True)
+    oecd_df.set_index('Date', inplace=True)
 
-# Export final CSV
-df.to_csv('data/processed/monthly_data.csv')
+    # Concatenate data
+    df = pd.concat([fred_df, oecd_df], axis=1)
+
+    # Export final CSV
+    df.to_csv('data/processed/monthly_data.csv')
+
+if __name__ == '__main__':
+    merge()
